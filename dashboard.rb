f7255a44e9aecaa8e86986c5e62d9bfd2c321828
Mia Gaulin <gaulin.mia@gmail.com>
2016-03-10 13:36:01 -0500

    Movies widget

--- dashboard.rb
+++ dashboard.rb
@@ -19,7 +19,17 @@ class Dashboard < Sinatra::Base
 
     response = HTTParty.get(url)
     @weather_data = JSON.parse(response.body)["currently"]
-    
+
+    events_key = ENV["GRACENOTE_API_KEY"]
+    events_zip = @geolocation.zip
+    date = Time.now.strftime("%Y-%m-%d")
+
+    url=("http://data.tmsapi.com/v1.1/movies/showings?startDate=#{date}&zip=#{events_zip}&api_key=#{events_key}")
+
+    response = HTTParty.get(url)
+    @movies = JSON.parse(response.body)
+
+    # binding.pry
     erb :dashboard
   end
 end
